d612c5e1f32c83c71fe6348e33a2b7e3a4f6c201
AbhishekKarunakaran <abhishekkarunakaran100@gmail.com>
2020-12-15 10:26:50 +0530

    Changes are made
    * The first condition is changed as it check that the filename ends with .cpp or .c
    * Added an else block to the first if, to exit the program when an invalid filename is entered

--- crun.sh
+++ crun.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-#bash script to compile and run c program
-if [[ $1 == *".cpp"* ]]
+#bash script to compile and run c and c++ programs
+if [[ $1 == *".cpp" ]]
 then
-    g++ $1
-elif [[ $1 == *".c"* ]]
+    g++ $1 
+    
+    #Can also compile a c program with g++ compiler
+    echo "Do you want to run the c++ program(y/n)?"
+
+    read ch
+elif [[ $1 == *".c" ]]
 then
     gcc $1
+    echo "Do you want to run the c program(y/n)?"
+
+    read ch
+else
+    echo "Invalid filename with extention"
+    exit
 fi
-
-echo "Do you want to run the c program(y/n)?"
-
-read ch
 
 if [ $ch == 'y' ] || [ $ch == 'Y' ]
 then
