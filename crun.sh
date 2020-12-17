17b4e4dd70cdccb51b1aa782cc2a5dd95081f43d
Abhishekkarunakaran <abhishekkarunakaran100@gmail.com>
2020-12-18 00:47:24 +0530

    Added compilation of Java

--- crun.sh
+++ crun.sh
@@ -9,6 +9,11 @@ fi
 
 input=${1}
 
+if [ ! -z "$2" ]
+then
+    arg=$2
+fi
+
 if test -f "a.out"
     then
         rm a.out
@@ -35,7 +40,55 @@ then
     else 
         exit
     fi
+elif [[ $input == *".java" ]]
+then
+    if [ -z "$arg" ]
+    then
+        class_name=`echo $input | cut -d'.' -f 1`
+        file_name="_crun_java_${class_name}_"
 
+        if test -d $file_name
+        then
+            rm -r $file_name
+        fi
+
+        mkdir $file_name
+        cp $input $file_name
+        cd $file_name
+
+        javac $input
+
+        java $class_name
+
+        cd ..
+        exit
+    else
+        class_name=`echo $input | cut -d'.' -f 1`
+        file_name="_crun_java_${class_name}_"
+
+        if test -d $file_name
+        then
+            rm -r $file_name
+        fi
+        mkdir $file_name
+        if [[ $arg == *".txt" ]]
+        then
+            cp $input $file_name
+            cp $arg $filename
+            cd $file_name
+
+            javac $input
+
+            java $class_name $arg
+        else
+            cp $input $file_name
+            cd $file_name
+
+            javac $input
+
+            java $class_name $arg
+        fi
+    fi
 else
     echo "Invalid filename with extention"
     exit
